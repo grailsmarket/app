82ac41576d549bd0baa72b7c28bafc92c39b5ef0
Jan Žunec <encrypted@ethfollow.xyz>
2025-11-15 15:38:24 -0300

    fixing mobile scroll issues

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -8,7 +8,7 @@ const Marketplace = () => {
   return (
     <Suspense>
       <FilterProvider filterType='marketplace'>
-        <main className='relative overflow-hidden h-[100dvh]'>
+        <main className='relative overflow-hidden max-h-[100dvh]'>
           {/* <div className='absolute top-0 left-0 z-0 flex h-full w-screen -translate-y-56 items-center justify-between'>
           <Image
             src={heroBackground}
@@ -19,8 +19,8 @@ const Marketplace = () => {
           />
           <Image src={heroBackgroundRight} alt='hero-background' className='object-cover' width={600} height={1200} />
         </div> */}
-          <div className='relative z-10 mx-auto flex w-full flex-col gap-32 pt-20'>
-            <div className='px-md bg-background relative mx-auto flex h-[calc(100dvh-80px)] w-full flex-row gap-2 overflow-hidden sm:px-3 md:h-[calc(100dvh-80px)] lg:px-0 lg:pl-2'>
+          <div className='relative z-10 mx-auto flex w-full flex-col pt-20 max-h-[100dvh] overflow-hidden'>
+            <div className='px-md bg-background relative mx-auto flex h-[calc(100dvh-78px)] w-full flex-row gap-2 overflow-hidden sm:px-3 lg:px-0 lg:pl-2'>
               <FilterPanel />
               <div className='bg-tertiary hidden h-full w-0.5 lg:block' />
               <DomainPanel />

424566bb9c28ecc91ae8477322627a7d63ddd651
Jan Žunec <encrypted@ethfollow.xyz>
2025-12-19 19:32:03 +0100

    add view all to categories, change navbar logo

--- src/components/home/topCategories.tsx
+++ src/components/home/topCategories.tsx
@@ -3,6 +3,8 @@
 import CategoryRow from '@/app/categories/components/categoryRow'
 import { useCategories } from '@/components/filters/hooks/useCategories'
 import { cn } from '@/utils/tailwind'
+import { Arrow } from 'ethereum-identity-kit'
+import Link from 'next/link'
 import React from 'react'
 
 const TopCategories = () => {
@@ -10,7 +12,16 @@ const TopCategories = () => {
 
   return (
     <div className='w-full'>
-      <h2 className='font-sedan-sc p-xl text-center text-5xl md:text-6xl lg:text-left'>Top Categories</h2>
+      <div className='p-xl flex items-center justify-between'>
+        <h2 className='font-sedan-sc text-center text-5xl md:text-6xl lg:text-left'>Top Categories</h2>
+        <Link
+          href='/categories'
+          className='text-primary hover:text-primary/80 flex items-center gap-2 text-center text-2xl font-semibold md:text-left'
+        >
+          <p>View All</p>
+          <Arrow className='text-primary h-4 w-4 rotate-180' />
+        </Link>
+      </div>
       <div className='flex w-full flex-wrap justify-center gap-4 lg:flex-nowrap'>
         {categories?.slice(0, 3).map((category, index) => (
           <div

10b55864ecfec5b85c3d75bd155c81ef4b8df78b
0xthrpw <0xthrpw@gmail.com>
2026-01-28 23:49:38 -0500

    dev api url

--- src/constants/api/index.ts
+++ src/constants/api/index.ts
@@ -1,6 +1,6 @@
 // export const API_URL = 'https://grails-api.ethid.org/api/v1'
-// export const API_URL = 'https://grails-api-dev.ethid.org/api/v1'
-export const API_URL = 'http://localhost:3000/api/v1'
+export const API_URL = 'https://grails-api-dev.ethid.org/api/v1'
+// export const API_URL = 'http://localhost:3000/api/v1'
 export const SIWE_STATEMENT = 'Welcome to Grails!'
 
 export const DEFAULT_FETCH_LIMIT = 50

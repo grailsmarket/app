6e30ae0492f57c2502349cb27296f1e51934455a
Jan Žunec <encrypted@ethfollow.xyz>
2026-02-09 11:10:36 +0100

    change border color on profile details

--- src/app/profile/[user]/components/details.tsx
+++ src/app/profile/[user]/components/details.tsx
@@ -40,7 +40,7 @@ const Details: React.FC<Props> = ({ user }) => {
   })
 
   return (
-    <div className='border-neutral bg-secondary lg:bg-secondary flex w-full flex-col justify-between gap-2 p-2 sm:gap-4 md:p-4 lg:w-[380px] lg:border-l-2'>
+    <div className='border-tertiary bg-secondary flex w-full flex-col justify-between gap-2 p-2 sm:gap-4 md:p-4 lg:w-[380px] lg:border-l-2'>
       <div className='flex flex-col gap-4'>
         <h3 className='font-sedan-sc hidden text-3xl lg:block'>Account</h3>
         <div className='grid grid-cols-4 gap-y-4 lg:grid-cols-2'>

5a5b32c83b16e115ec4f496d9ea5a2d7d3980df4
Aaron-Desktop-K <24273561+caveman-eth@users.noreply.github.com>
2026-02-18 00:03:49 +0200

    Revert "Point API_URL to development backend - TEMP"
    This reverts commit bc9b6c99c72ffeca80de08537cfa96b40aceabb7.

--- src/constants/api/index.ts
+++ src/constants/api/index.ts
@@ -1,6 +1,4 @@
-// **DEV OVERRIDE ACTIVE**: API URL is currently pointed to the development backend.
-export const API_URL = 'https://grails-api-dev.up.railway.app/api/v1'
-
+export const API_URL = 'https://api.grails.app/api/v1'
 export const SIWE_STATEMENT = 'Welcome to Grails!'
 
 export const DEFAULT_FETCH_LIMIT = 50

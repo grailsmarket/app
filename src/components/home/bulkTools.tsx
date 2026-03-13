9c38095f8381ad175eb5c175d50e68d76b877e48
Jan Žunec <encrypted@ethfollow.xyz>
2026-03-13 15:44:29 +0100

    update home page bulk tools section

--- src/components/home/bulkTools.tsx
+++ src/components/home/bulkTools.tsx
@@ -6,6 +6,8 @@ import search from 'public/icons/search-primary.svg'
 import listings from 'public/icons/tag.svg'
 import extend from 'public/icons/extend.svg'
 import transfer from 'public/icons/transfer-primary.svg'
+import register from 'public/icons/registration-primary.svg'
+import editRecords from 'public/icons/pencil-primary.svg'
 import bulkToolsImage from 'public/art/bulk-tools.svg'
 import { setSearchModalOpen } from '@/state/reducers/modals/searchModal'
 import { useUserContext } from '@/context/user'
@@ -16,24 +18,35 @@ import { setBulkSelectIsSelecting } from '@/state/reducers/modals/bulkSelectModa
 
 const bulkTools = [
   {
-    title: 'Bulk Search',
-    description: 'Up to 10k search terms separated by commas or spaces.',
-    icon: <Image src={search} alt='Search' width={20} height={20} />,
+    title: 'Register',
+    description: 'Register multiple names in one transaction. Set custom durations for each name.',
+    icon: <Image src={register} alt='Register' width={24} height={24} />,
   },
+
   {
-    title: 'Bulk Listings',
+    title: 'Listings',
     description: 'List multiple names at once with different prices.',
     icon: <Image src={listings} alt='Listings' width={20} height={20} />,
   },
   {
-    title: 'Bulk Extend',
+    title: 'Extend',
     description: 'Renew multiple names at once, even from an account that’s not the owner.',
     icon: <Image src={extend} alt='Extend' width={24} height={24} />,
   },
   {
-    title: 'Bulk Transfer',
+    title: 'Transfer',
     description: 'Transfer multiple names in one transaction.',
     icon: <Image src={transfer} alt='Transfer' width={24} height={24} />,
+  },
+  {
+    title: 'Search',
+    description: 'Up to 10k search terms separated by commas or spaces.',
+    icon: <Image src={search} alt='Search' width={20} height={20} />,
+  },
+  {
+    title: 'Edit Records',
+    description: 'Edit records for multiple names. Set records for all at once or individually.',
+    icon: <Image src={editRecords} alt='Edit Records' width={24} height={24} />,
   },
 ]
 
@@ -59,7 +72,7 @@ const BulkTools = () => {
   }
   return (
     <div className='flex w-full flex-col justify-between gap-8 xl:flex-row'>
-      <div className='flex flex-col items-center gap-7 lg:items-start xl:min-w-[596px]'>
+      <div className='flex w-full flex-col items-center gap-7 lg:items-start'>
         <div className='flex flex-col gap-4'>
           <h2 className='font-sedan-sc text-center text-5xl md:text-6xl lg:text-left'>Bulk Tools</h2>
           <p className='text-center font-semibold sm:text-2xl lg:text-left'>
@@ -69,7 +82,7 @@ const BulkTools = () => {
         <div className='flex flex-row flex-wrap gap-4'>
           {bulkTools.map((tool) => (
             <div
-              className='hover:bg-primary/15 border-primary p-lg flex w-full cursor-pointer flex-col gap-2 rounded-md border-1 transition-colors sm:w-[calc(50%-8px)] lg:w-[calc(25%-12px)] xl:w-[290px]'
+              className='hover:bg-primary/15 border-primary p-lg flex w-full cursor-pointer flex-col gap-2 rounded-md border-1 transition-colors sm:w-[calc(50%-8px)] lg:w-[calc(33%-12px)]'
               key={tool.title}
               onClick={() => handleToolClick(tool)}
             >
@@ -82,13 +95,13 @@ const BulkTools = () => {
           ))}
         </div>
       </div>
-      <Image
+      {/* <Image
         src={bulkToolsImage}
         alt='Bulk Tools'
         width={1200}
         height={600}
         className='mx-auto h-fit w-full max-w-[800px] lg:mx-0 xl:w-1/2'
-      />
+      /> */}
     </div>
   )
 }

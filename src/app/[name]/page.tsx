970101181ba6747525553ce981a3000dbb24f22c
encryptedDegen <encrypted@ethfollow.xyz>
2026-05-18 09:59:18 +0000

    remove stale name prefetch code

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -8,11 +8,8 @@ import ServerPanels from './components/serverPanels'
 import { beautifyName } from '@/lib/ens'
 import { notFound } from 'next/navigation'
 import { fetchNameDetails } from '@/api/name/details'
-import { fetchNameOffers } from '@/api/name/offers'
-import { fetchNameRoles } from '@/api/name/roles'
 import { ONE_MINUTE } from '@/constants/time'
-import { DomainOfferType, MarketplaceDomainType } from '@/types/domains'
-import { RolesType } from '@/types/api'
+import { MarketplaceDomainType } from '@/types/domains'
 
 interface Props {
   params: Promise<{ name: string }>
@@ -72,21 +69,9 @@ const Name = async (props: Props) => {
       queryFn: () => fetchNameDetails(normalizedName),
       staleTime: PREFETCH_STALE_TIME,
     }),
-    // queryClient.prefetchQuery({
-    //   queryKey: ['name', 'offers', normalizedName],
-    //   queryFn: () => fetchNameOffers(normalizedName),
-    //   staleTime: PREFETCH_STALE_TIME,
-    // }),
-    // queryClient.prefetchQuery({
-    //   queryKey: ['name', 'roles', normalizedName],
-    //   queryFn: () => fetchNameRoles(normalizedName),
-    //   staleTime: PREFETCH_STALE_TIME,
-    // }),
   ])
 
   const nameDetails = queryClient.getQueryData<MarketplaceDomainType>(['name', 'details', normalizedName])
-  // const nameOffers = queryClient.getQueryData<DomainOfferType[]>(['name', 'offers', normalizedName])
-  // const roles = queryClient.getQueryData<RolesType | null>(['name', 'roles', normalizedName])
 
   return (
     <main className='min-h-[calc(100dvh-56px)] w-full pb-4 sm:px-4 md:min-h-[calc(100dvh-78px)]'>

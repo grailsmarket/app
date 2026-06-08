69fa59f4cddc1f835158a385ec5e32a832607963
encryptedDegen <encrypted@ethfollow.xyz>
2026-06-08 21:58:25 +0000

    make home tools follow app width
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- src/components/home/twitterBot.tsx
+++ src/components/home/twitterBot.tsx
@@ -37,32 +37,34 @@ const TwitterBot = () => {
   }
 
   return (
-    <div className='flex w-full flex-col-reverse gap-10 md:flex-row md:items-center md:justify-center'>
+    <div className='flex w-full flex-col-reverse gap-10 @[48rem]/app:flex-row @[48rem]/app:items-center @[48rem]/app:justify-center'>
       <iframe
         loading='lazy'
-        className='mx-auto aspect-[400/642] w-full max-w-[400px] md:mx-0 md:w-[340px] lg:w-[380px]'
+        className='mx-auto aspect-[400/642] w-full max-w-[400px] @[48rem]/app:mx-0 @[48rem]/app:w-[340px] @[64rem]/app:w-[380px]'
         src='https://platform.twitter.com/embed/Tweet.html?frame=false&hideCard=false&hideThread=true&id=1990916043254972604&origin=YOUR_DOMAIN_HERE&theme=dark&width=500px'
         scrolling='no'
       ></iframe>
       <div className='flex flex-col gap-6'>
         <div className='flex flex-col gap-3'>
-          <h2 className='font-sedan-sc text-center text-5xl sm:text-6xl md:text-left'>Twitter Bot</h2>
-          <p className='text-center text-xl font-semibold md:text-left'>
+          <h2 className='font-sedan-sc text-center text-5xl @[40rem]/app:text-6xl @[48rem]/app:text-left'>
+            Twitter Bot
+          </h2>
+          <p className='text-center text-xl font-semibold @[48rem]/app:text-left'>
             Don’t miss a major sale, offer, or registration. Join the conversation on 𝕏.
           </p>
         </div>
-        <div className='flex flex-col items-center gap-6 md:items-start'>
+        <div className='flex flex-col items-center gap-6 @[48rem]/app:items-start'>
           {twitterBotDetails.map((detail) => (
-            <div key={detail.title} className='flex flex-col items-center gap-2 md:items-start'>
+            <div key={detail.title} className='flex flex-col items-center gap-2 @[48rem]/app:items-start'>
               <div className='flex flex-row items-center gap-2'>
                 {detail.icon}
-                <h3 className='text-primary text-center text-2xl font-bold md:text-left'>{detail.title}</h3>
+                <h3 className='text-primary text-center text-2xl font-bold @[48rem]/app:text-left'>{detail.title}</h3>
               </div>
-              <p className='text-center font-medium md:text-left'>{detail.description}</p>
+              <p className='text-center font-medium @[48rem]/app:text-left'>{detail.description}</p>
             </div>
           ))}
         </div>
-        <PrimaryButton className='px-xl mx-auto mt-2 h-fit! py-3 text-xl md:mx-0' onClick={handleFollow}>
+        <PrimaryButton className='px-xl mx-auto mt-2 h-fit! py-3 text-xl @[48rem]/app:mx-0' onClick={handleFollow}>
           Follow on &nbsp;𝕏
         </PrimaryButton>
       </div>

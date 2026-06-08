1f67b9a39b7f987b30325b4eaf8fadef89d7cd09
encryptedDegen <encrypted@ethfollow.xyz>
2026-06-08 21:58:24 +0000

    make name metrics follow app width
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- src/app/[name]/components/activity.tsx
+++ src/app/[name]/components/activity.tsx
@@ -15,13 +15,13 @@ const ActivityPanel: React.FC<Props> = ({ name }) => {
   return (
     <div
       className={cn(
-        'sm:border-tertiary bg-secondary pt-lg flex w-full flex-col gap-1 sm:rounded-lg sm:border-2 lg:gap-2',
+        '@[40rem]/app:border-tertiary bg-secondary pt-lg flex w-full flex-col gap-1 @[40rem]/app:rounded-lg @[40rem]/app:border-2 @[64rem]/app:gap-2',
         isActivityEmpty && 'pb-6'
       )}
       style={{ maxHeight: '390px', overflow: 'hidden' }}
     >
-      <h2 className='px-lg xl:px-xl font-sedan-sc text-3xl'>Activity</h2>
-      <div className='px-md z-0 w-full overflow-y-auto sm:px-0'>
+      <h2 className='px-lg @[80rem]/app:px-xl font-sedan-sc text-3xl'>Activity</h2>
+      <div className='px-md z-0 w-full overflow-y-auto @[40rem]/app:px-0'>
         <Activity
           paddingBottom='0px'
           activity={activity}

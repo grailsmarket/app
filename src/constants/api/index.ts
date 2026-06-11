4519523bdf68188c551f1ea73190d1dc550d345d
Jan Žunec <encrypted@ethfollow.xyz>
2026-06-12 00:44:36 +0200

    remove comments

--- src/constants/api/index.ts
+++ src/constants/api/index.ts
@@ -1,5 +1,3 @@
-// export const API_URL = 'https://api.grails.app/api/v1'
-// export const API_URL = 'https://grails-api-dev.up.railway.app/api/v1'
 export const API_URL = process.env.NEXT_PUBLIC_GRAILS_API_URL || 'https://api.grails.app/api/v1'
 export const SIWE_STATEMENT = 'Welcome to Grails!'
 
